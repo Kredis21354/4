da3fec1d338b23462539d7ba9f57b1021c96a67f
Kredis21354 <125788986+Kredis21354@users.noreply.github.com>
2023-02-26 09:27:07 +0000

    added 2student

--- main.py
+++ main.py
@@ -23,123 +23,19 @@ class Teacher(Person):
     super().__init__("Teacher")
 
   def give_grade(self, human):
-    human.grade += 1
+    human.grade = 12
 
 student = Student()
-
-
-
-
+student2 = Student()
 teacher = Teacher()
 
+print('оцінка першого студента до',student.grade)
 teacher.give_grade(student)
+print('оцінка першого студента після',student.grade)
 
-print(student.grade)
-
-teacher.give_grade(student)
-
-print(student.grade)
-
-teacher.give_grade(student)
-
-print(student.grade)
-
-teacher.give_grade(student)
-
-print(student.grade)
-
-teacher.give_grade(student)
-
-print(student.grade)
-
-teacher.give_grade(student)
-
-print(student.grade)
-
-teacher.give_grade(student)
-
-print(student.grade)
-
-teacher.give_grade(student)
-
-print(student.grade)
-
-teacher.give_grade(student)
-
-print(student.grade)
-
-teacher.give_grade(student)
-
-print(student.grade)
-
-teacher.give_grade(student)
-
-print(student.grade)
-
-teacher.give_grade(student)
-
-print(student.grade)
-
-
-
-
-
-
-
-
-
-
-teacher.teach()
-
-print(teacher.salary)
-
-teacher.teach()
-
-print(teacher.salary)
-
-teacher.teach()
-
-print(teacher.salary)
-
-teacher.teach()
-
-print(teacher.salary)
-
-teacher.teach()
-
-print(teacher.salary)
-
-teacher.teach()
-
-print(teacher.salary)
-
-teacher.teach()
-
-print(teacher.salary)
-
-teacher.teach()
-
-print(teacher.salary)
-
-teacher.teach()
-
-print(teacher.salary)
-
-teacher.teach()
-
-print(teacher.salary)
-
-teacher.teach()
-
-print(teacher.salary)
-
-teacher.teach()
-
-print(teacher.salary)
-
-teacher.teach()
-
-print(teacher.salary)
+print('оцінка другогостудента до',student2.grade)
+teacher.give_grade(student2)
+print('оцінка другогостудента після',student2.grade)
 
 
 
