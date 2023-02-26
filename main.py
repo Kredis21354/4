ba7731919167021543e627a706f56d2a5e838629
Kredis21354 <125788986+Kredis21354@users.noreply.github.com>
2023-02-26 09:11:34 +0000

    added grades and salary

--- main.py
+++ main.py
@@ -1,13 +1,131 @@
 class Person:
   name = "Name"
 
-def __init__(self, name):
-  self.name = name
+  def __init__(self, name):
+    self.name = name
 
 class Student(Person):
   grade = 0
-
+  def study(self):
+    self.grade += 1
+  
+  def __init__(self):
+    super().__init__("Student")
+  
 class Teacher(Person):
   salary = 0
   student = []
+
+  def teach(self):
+    self.salary += 500
   
+  def __init__(self):
+    super().__init__("Teacher")
+
+student = Student()
+
+
+student.study()
+
+print(student.grade)
+
+student.study()
+
+print(student.grade)
+
+student.study()
+
+print(student.grade)
+
+student.study()
+
+print(student.grade)
+
+student.study()
+
+print(student.grade)
+
+student.study()
+
+print(student.grade)
+
+student.study()
+
+print(student.grade)
+
+student.study()
+
+print(student.grade)
+
+student.study()
+
+print(student.grade)
+
+student.study()
+
+print(student.grade)
+
+student.study()
+
+print(student.grade)
+
+student.study()
+
+print(student.grade)
+
+teacher = Teacher()
+
+teacher.teach()
+
+print(teacher.salary)
+
+teacher.teach()
+
+print(teacher.salary)
+
+teacher.teach()
+
+print(teacher.salary)
+
+teacher.teach()
+
+print(teacher.salary)
+
+teacher.teach()
+
+print(teacher.salary)
+
+teacher.teach()
+
+print(teacher.salary)
+
+teacher.teach()
+
+print(teacher.salary)
+
+teacher.teach()
+
+print(teacher.salary)
+
+teacher.teach()
+
+print(teacher.salary)
+
+teacher.teach()
+
+print(teacher.salary)
+
+teacher.teach()
+
+print(teacher.salary)
+
+teacher.teach()
+
+print(teacher.salary)
+
+teacher.teach()
+
+print(teacher.salary)
+
+
+
